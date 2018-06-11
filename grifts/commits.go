d8c8e9a43f50d0907ac87e29208fcdc5ca3b75e0
Obi Knows <sam.nnodim@gmail.com>
2018-06-11 00:25:07 -0700

    added tachyons back. got the HEX logo and a new top logo for the home screen

--- grifts/commits.go
+++ grifts/commits.go
@@ -10,7 +10,9 @@ var _ = Namespace("code", func() {
 
 	Desc("commits", "Gets json array of a year of github commit data for the calendar")
 	Add("commits", func(c *Context) error {
+
 		fmt.Println("Lets get these commits...")
+
 		return nil
 	})
 
