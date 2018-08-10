94cb2abae3188e82acc2e38a2f255df0c2ec9b5b
obi <obiknows88@gmail.com>
2018-08-10 13:55:05 -0700

    better looking entry page

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,8 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.min.js");
 $(() => {
-  
+  // Setup tooltips everywhere
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
 });
